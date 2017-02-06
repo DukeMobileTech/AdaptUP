8741fe917c0def20d116babe815e4f87f7566b8e
Leonard Ng'eno <dranoel.bravo@gmail.com>
2017-02-06 12:58:50 -0500

    adhere to jss

--- server.js
+++ server.js
@@ -1,66 +1,66 @@
-var express = require('express'),
-    app = express(),
-    ejs = require('ejs'),
-    fs = require('fs'),
-    winston = require('winston'),
-    expressWinston = require('express-winston'),
-    bodyParser = require('body-parser'),
-    passport = require('passport'),
-    yaml = require('js-yaml');
-var routes = require('./routes');
-const PORT = 5000;
+var express = require('express')
+var app = express()
+var fs = require('fs')
+var path = require('path')
+var winston = require('winston')
+var expressWinston = require('express-winston')
+var bodyParser = require('body-parser')
+var passport = require('passport')
+var yaml = require('js-yaml')
+var routes = require('./routes')
+const PORT = 5000
 
-expressWinston.requestWhitelist = ['url', 'method', 'originalUrl', 'query'];
+expressWinston.requestWhitelist = ['url', 'method', 'originalUrl', 'query']
 app.use(expressWinston.logger({
-    transports: [
-        new winston.transports.Console({
-            json: true,
-            colorize: true,
-            timestamp: true,
-            level: 'info'
-        })
-    ]
-}));
-// router needs to go after the logger 
-app.use(routes.router);
+  transports: [
+    new winston.transports.Console({
+      json: true,
+      colorize: true,
+      timestamp: true,
+      level: 'info'
+    })
+  ]
+}))
+// router needs to go after the logger
+app.use(routes.router)
 // errorLogger needs to go after the router
 app.use(expressWinston.errorLogger({
-    transports: [
-        new winston.transports.Console({
-            json: true,
-            colorize: true,
-            timestamp: true,
-            level: 'info'
-        })
-    ]
-}));
+  transports: [
+    new winston.transports.Console({
+      json: true,
+      colorize: true,
+      timestamp: true,
+      level: 'info'
+    })
+  ]
+}))
 
-app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
-app.use(passport.initialize());
-app.use('/adaptup', routes.router);
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, '/public')))
+app.use(passport.initialize())
+app.use('/adaptup', routes.router)
 
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.get('/', function(req, res) {
-    res.redirect('/adaptup');
-});
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/views'))
+app.get('/', function (req, res) {
+  res.redirect('/adaptup')
+})
 
-passport.use('jawbone', routes.strategy.jawboneStrategy);
+passport.use('jawbone', routes.strategy.jawboneStrategy)
 
-if (app.settings.env == 'production') {
-    var http = require('http');
-    http.createServer(app).listen(PORT, function() {
-        console.log('AdaptUP ' + app.settings.env + ' server listening on ' + PORT);
-    });
+if (app.settings.env === 'production') {
+  var http = require('http')
+  http.createServer(app).listen(PORT, function () {
+    console.log('AdaptUP ' + app.settings.env + ' server listening on ' + PORT)
+  })
 } else {
-    var https = require('https');
-    var settings = yaml.safeLoad(fs.readFileSync('config/settings.yml', 'utf8'));
-    var sslOptions = {
-        key: fs.readFileSync(settings['serverKey']),
-        cert: fs.readFileSync(settings['serverCert'])
-    };
-    https.createServer(sslOptions, app).listen(PORT, function() {
-        console.log('AdaptUP ' + app.settings.env + ' server listening on ' + PORT);
-    });
+  var https = require('https')
+  var settings = yaml.safeLoad(fs.readFileSync('config/settings.yml', 'utf8'))
+  var sslOptions = {
+    key: fs.readFileSync(settings['serverKey']),
+    cert: fs.readFileSync(settings['serverCert'])
+  }
+  https.createServer(sslOptions, app).listen(PORT, function () {
+    console.log('AdaptUP ' + app.settings.env + ' server listening on ' + PORT)
+  })
 }
--- strategy.js
+++ strategy.js
@@ -1,281 +1,282 @@
-var fs = require('fs'),
-    yaml = require('js-yaml'),
-    jsonToCsv = require('json-2-csv'),
-    asyncFun = require('async'),
-    JawboneStrategy = require('passport-oauth').OAuth2Strategy;
-var settings = yaml.safeLoad(fs.readFileSync('config/settings.yml', 'utf8'));
+var fs = require('fs')
+var yaml = require('js-yaml')
+var jsonToCsv = require('json-2-csv')
+var asyncFun = require('async')
+var JawboneStrategy = require('passport-oauth').OAuth2Strategy
+var settings = yaml.safeLoad(fs.readFileSync('config/settings.yml', 'utf8'))
 var jawboneAuth = {
-    clientID: settings['clientID'],
-    clientSecret: settings['clientSecret'],
-    authorizationURL: 'https://jawbone.com/auth/oauth2/auth',
-    tokenURL: 'https://jawbone.com/auth/oauth2/token',
-    callbackURL: settings['callbackURL']
-};
-var startDate, endDate, userEmail, emaID, dataDir, up, summaryCounter, fileCounter, moveCounter, sleepCounter;
-var dataSummary = [],
-    moveIdentifiers = [],
-    sleepIdentifiers = [],
-    moveTicksData = [],
-    sleepTicksData = [];
+  clientID: settings['clientID'],
+  clientSecret: settings['clientSecret'],
+  authorizationURL: 'https://jawbone.com/auth/oauth2/auth',
+  tokenURL: 'https://jawbone.com/auth/oauth2/token',
+  callbackURL: settings['callbackURL']
+}
+var startDate, endDate, userEmail, emaID, dataDir, up, summaryCounter, fileCounter, moveCounter, sleepCounter
+var dataSummary = []
+var moveIdentifiers = []
+var sleepIdentifiers = []
+var moveTicksData = []
+var sleepTicksData = []
 const HR_HEADERS = ['user_xid', 'user_email', 'ema_id', 'time_accessed',
-    'xid', 'title', 'place_lon', 'place_lat', 'place_acc', 'place_name',
-    'time_created', 'time_updated', 'date', 'resting_heartrate', 'details.tz',
-    'details.sunrise', 'details.sunset'
-];
+  'xid', 'title', 'place_lon', 'place_lat', 'place_acc', 'place_name',
+  'time_created', 'time_updated', 'date', 'resting_heartrate', 'details.tz',
+  'details.sunrise', 'details.sunset'
+]
 const WORKOUT_HEADERS = ['user_xid', 'user_email', 'ema_id', 'time_accessed',
-    'xid', 'title', 'type', 'sub_type', 'place_lon', 'place_lat', 'place_acc',
-    'place_name', 'time_created', 'time_updated', 'time_completed', 'date',
-    'details.steps', 'details.time', 'details.tz', 'details.bg_active_time',
-    'details.calories', 'details.bmr_calories', 'details.bmr',
-    'details.bg_calories', 'details.meters', 'details.km', 'details.intensity'
-];
+  'xid', 'title', 'type', 'sub_type', 'place_lon', 'place_lat', 'place_acc',
+  'place_name', 'time_created', 'time_updated', 'time_completed', 'date',
+  'details.steps', 'details.time', 'details.tz', 'details.bg_active_time',
+  'details.calories', 'details.bmr_calories', 'details.bmr',
+  'details.bg_calories', 'details.meters', 'details.km', 'details.intensity'
+]
 const MOVE_HEADERS = ['user_xid', 'user_email', 'ema_id', 'time_accessed',
-    'xid', 'title', 'type', 'time_created', 'time_updated', 'time_completed',
-    'date', 'details.distance', 'details.km', 'details.steps',
-    'details.active_time', 'details.longest_active', 'details.inactive_time',
-    'details.longest_idle', 'details.calories', 'details.bmr_day', 'details.bmr',
-    'details.bg_calories', 'details.wo_calories', 'details.wo_time',
-    'details.wo_active_time', 'details.wo_count', 'details.wo_longest',
-    'details.sunrise', 'details.sunset', 'details.tz'
-];
+  'xid', 'title', 'type', 'time_created', 'time_updated', 'time_completed',
+  'date', 'details.distance', 'details.km', 'details.steps',
+  'details.active_time', 'details.longest_active', 'details.inactive_time',
+  'details.longest_idle', 'details.calories', 'details.bmr_day', 'details.bmr',
+  'details.bg_calories', 'details.wo_calories', 'details.wo_time',
+  'details.wo_active_time', 'details.wo_count', 'details.wo_longest',
+  'details.sunrise', 'details.sunset', 'details.tz'
+]
 const SLEEP_HEADERS = ['user_xid', 'user_email', 'ema_id', 'time_accessed',
-    'xid', 'title', 'sub_type', 'time_created', 'time_completed', 'date',
-    'place_lat', 'place_lon', 'place_acc', 'place_name',
-    'details.smart_alarm_fire', 'details.awake_time', 'details.asleep_time',
-    'details.awakenings', 'details.rem', 'details.light', 'details.deep',
-    'details.awake', 'details.duration', 'details.tz'
-];
+  'xid', 'title', 'sub_type', 'time_created', 'time_completed', 'date',
+  'place_lat', 'place_lon', 'place_acc', 'place_name',
+  'details.smart_alarm_fire', 'details.awake_time', 'details.asleep_time',
+  'details.awakenings', 'details.rem', 'details.light', 'details.deep',
+  'details.awake', 'details.duration', 'details.tz'
+]
 const MOVE_TICKS_HEADERS = ['user_xid', 'user_email', 'ema_id', 'time_accessed',
-    'distance', 'time_completed', 'active_time', 'calories', 'steps', 'time', 'speed'
-];
-const SLEEP_TICKS_HEADERS = ['user_xid', 'user_email', 'ema_id', 'time_accessed', 'depth', 'time'];
-const SUMMARY_HEADERS = ['date', 'resting_heartrate', 'step_count', 'sleep_duration'];
+  'distance', 'time_completed', 'active_time', 'calories', 'steps', 'time', 'speed'
+]
+const SLEEP_TICKS_HEADERS = ['user_xid', 'user_email', 'ema_id', 'time_accessed', 'depth', 'time']
+const SUMMARY_HEADERS = ['date', 'resting_heartrate', 'step_count', 'sleep_duration']
 
-exports.setStartDate = function(date) {
-    startDate = date;
+exports.setStartDate = function (date) {
+  startDate = date
 }
 
-exports.setEndDate = function(date) {
-    endDate = date;
+exports.setEndDate = function (date) {
+  endDate = date
 }
 
-exports.setUserEmail = function(email) {
-    userEmail = email;
+exports.setUserEmail = function (email) {
+  userEmail = email
 }
 
-exports.setEmaId = function(id) {
-    emaID = id;
+exports.setEmaId = function (id) {
+  emaID = id
 }
 
-exports.setDataDir = function(dir) {
-    dataDir = dir;
+exports.setDataDir = function (dir) {
+  dataDir = dir
 }
 
-exports.jawboneStrategy = new JawboneStrategy(jawboneAuth, function(token, refreshToken, profile, done) {
-    var options = {
-        access_token: token,
-        client_id: jawboneAuth.clientID,
-        client_secret: jawboneAuth.clientSecret
-    };
-    up = require('jawbone-up')(options);
-    var params = {
-        start_time: startDate,
-        end_time: endDate,
-        limit: 1000000
-    };
-    summaryCounter = 0;
-    fileCounter = 0;
-    moveCounter = 0;
-    sleepCounter = 0;
+exports.jawboneStrategy = new JawboneStrategy(jawboneAuth, function (token, refreshToken, profile, done) {
+  var options = {
+    access_token: token,
+    client_id: jawboneAuth.clientID,
+    client_secret: jawboneAuth.clientSecret
+  }
+  up = require('jawbone-up')(options)
+  var params = {
+    start_time: startDate,
+    end_time: endDate,
+    limit: 1000000
+  }
+  summaryCounter = 0
+  fileCounter = 0
+  moveCounter = 0
+  sleepCounter = 0
 
-    up.heartrates.get(params, function(err, body) {
-        parseData(HR_HEADERS, 'heartrates.csv', err, body, false);
-    });
+  up.heartrates.get(params, function (err, body) {
+    parseData(HR_HEADERS, 'heartrates.csv', err, body, false)
+  })
 
-    up.workouts.get(params, function(err, body) {
-        parseData(WORKOUT_HEADERS, 'workouts.csv', err, body, false);
-    });
+  up.workouts.get(params, function (err, body) {
+    parseData(WORKOUT_HEADERS, 'workouts.csv', err, body, false)
+  })
 
-    up.moves.get(params, function(err, body) {
-        parseData(MOVE_HEADERS, 'moves.csv', err, body, true);
-    });
+  up.moves.get(params, function (err, body) {
+    parseData(MOVE_HEADERS, 'moves.csv', err, body, true)
+  })
 
-    up.sleeps.get(params, function(err, body) {
-        parseData(SLEEP_HEADERS, 'sleep.csv', err, body, true);
-    });
+  up.sleeps.get(params, function (err, body) {
+    parseData(SLEEP_HEADERS, 'sleep.csv', err, body, true)
+  })
 
-    asyncFun.whilst(
-        function() {
-            return fileCounter < 7;
-        },
-        function(callback) {
-            setTimeout(function() {
-                callback(null, fileCounter);
-            }, 1000);
-        },
-        function(err, n) {
-            if (err) {
-                console.log(new Date() + ': Async Task Error : ' + err);
-            }
-            return done(null, {
-                items: dataSummary,
-                user: userEmail
-            }, console.log(new Date() + ': summary data ready!'));
-        }
-    );
-});
+  asyncFun.whilst(
+    function () {
+      return fileCounter < 7
+    },
+    function (callback) {
+      setTimeout(function () {
+        callback(null, fileCounter)
+      }, 1000)
+    },
+    function (err, n) {
+      if (err) {
+        console.log(new Date() + ': Async Task Error : ' + err)
+      }
+      return done(null, {
+        items: dataSummary,
+        user: userEmail
+      }, console.log(new Date() + ': summary data ready!'))
+    }
+  )
+})
 
-function parseData(headers, file, err, body, getTicks) {
+function parseData (headers, file, err, body, getTicks) {
+  if (err) {
+    console.log(new Date() + ': Error Receiving Jawbone UP Data: ' + err)
+  } else {
+    var dataItems = JSON.parse(body).data.items
+    if (dataItems) {
+      appendExtraItems(dataItems, body, getTicks, file)
+      convertAndFlush(dataItems, headers, file, true)
+    }
+  }
+}
+
+function appendExtraItems (dataItems, body, getTicks, filename) {
+  for (var k = 0; k < dataItems.length; k++) {
+    dataItems[k]['user_xid'] = JSON.parse(body).meta['user_xid']
+    dataItems[k]['time_accessed'] = JSON.parse(body).meta['time']
+    dataItems[k]['user_email'] = userEmail
+    dataItems[k]['ema_id'] = emaID
+    dataItems[k]['title'] = dataItems[k]['title'].replace(',', '')
+    if (getTicks) {
+      if (filename === 'moves.csv') {
+        moveIdentifiers.push(dataItems[k]['xid'])
+      } else if (filename === 'sleep.csv') {
+        sleepIdentifiers.push(dataItems[k]['xid'])
+      }
+    }
+  }
+  getActivityTicks(filename)
+}
+
+function convertAndFlush (dataArray, headers, file, summary) {
+  jsonToCsv.json2csv(dataArray, function (err, csv) {
     if (err) {
-        console.log(new Date() + ': Error Receiving Jawbone UP Data: ' + err);
+      console.log(new Date() + ': Error converting json to csv: ' + err)
     } else {
-        var dataItems = JSON.parse(body).data.items;
-        if (dataItems) {
-            appendExtraItems(dataItems, body, getTicks, file);
-            convertAndFlush(dataItems, headers, file, true);
-        }
-    }
-}
-
-function appendExtraItems(dataItems, body, getTicks, filename) {
-    for (var k = 0; k < dataItems.length; k++) {
-        dataItems[k]['user_xid'] = JSON.parse(body).meta['user_xid'];
-        dataItems[k]['time_accessed'] = JSON.parse(body).meta['time'];
-        dataItems[k]['user_email'] = userEmail;
-        dataItems[k]['ema_id'] = emaID;
-        dataItems[k]['title'] = dataItems[k]['title'].replace(',', '');
-        if (getTicks) {
-            if (filename === 'moves.csv') {
-                moveIdentifiers.push(dataItems[k]['xid']);
-            } else if (filename === 'sleep.csv') {
-                sleepIdentifiers.push(dataItems[k]['xid']);
-            }
-        }
-    }
-    getActivityTicks(filename);
-}
-
-function convertAndFlush(dataArray, headers, file, summary) {
-    jsonToCsv.json2csv(dataArray, function(err, csv) {
+      fs.writeFile(dataDir + file, csv, function (err) {
         if (err) {
-            console.log(new Date() + ': Error converting json to csv: ' + err)
+          console.log(new Date() + ': Error writing data to file: ' + err)
         } else {
-            fs.writeFile(dataDir + file, csv, function(err) {
-                if (err) {
-                    console.log(new Date() + ': Error writing data to file: ' + err);
-                } else {
-                    if (summary) {
-                        createSummaryObjects(dataArray);
-                    }
-                    fileCounter++;
-                }
-            });
+          if (summary) {
+            createSummaryObjects(dataArray)
+          }
+          fileCounter++
         }
-    }, {
-        KEYS: headers
-    });
-}
-
-function getActivityTicks(filename) {
-    if (filename === 'moves.csv') {
-        for (var k = 0; k < moveIdentifiers.length; k++) {
-            up.moves.ticks({
-                xid: moveIdentifiers[k]
-            }, function(error, body) {
-                moveCounter++;
-                parseTicksData(error, body, MOVE_TICKS_HEADERS, 'move_ticks.csv', moveCounter);
-            });
-        }
-    } else if (filename === 'sleep.csv') {
-        for (var k = 0; k < sleepIdentifiers.length; k++) {
-            up.sleeps.ticks({
-                xid: sleepIdentifiers[k]
-            }, function(error, body) {
-                sleepCounter++;
-                parseTicksData(error, body, SLEEP_TICKS_HEADERS, 'sleep_ticks.csv', sleepCounter);
-            });
-        }
+      })
     }
+  }, {
+    KEYS: headers
+  })
 }
 
-function parseTicksData(err, body, headers, filename, index) {
-    if (err) {
-        console.log(new Date() + ': Error Receiving Jawbone UP Data: ' + err);
+function getActivityTicks (filename) {
+  if (filename === 'moves.csv') {
+    for (var k = 0; k < moveIdentifiers.length; k++) {
+      up.moves.ticks({
+        xid: moveIdentifiers[k]
+      }, function (error, body) {
+        moveCounter++
+        parseTicksData(error, body, MOVE_TICKS_HEADERS, 'move_ticks.csv', moveCounter)
+      })
+    }
+  } else if (filename === 'sleep.csv') {
+    for (var i = 0; i < sleepIdentifiers.length; i++) {
+      up.sleeps.ticks({
+        xid: sleepIdentifiers[i]
+      }, function (error, body) {
+        sleepCounter++
+        parseTicksData(error, body, SLEEP_TICKS_HEADERS, 'sleep_ticks.csv', sleepCounter)
+      })
+    }
+  }
+}
+
+function parseTicksData (err, body, headers, filename, index) {
+  if (err) {
+    console.log(new Date() + ': Error Receiving Jawbone UP Data: ' + err)
+  } else {
+    var dataItems = JSON.parse(body).data.items
+    if (dataItems) {
+      for (var j = 0; j < dataItems.length; j++) {
+        dataItems[j]['user_xid'] = JSON.parse(body).meta['user_xid']
+        dataItems[j]['time_accessed'] = JSON.parse(body).meta['time']
+        dataItems[j]['user_email'] = userEmail
+        dataItems[j]['ema_id'] = emaID
+        if (filename === 'sleep_ticks.csv') {
+          sleepTicksData.push(dataItems[j])
+        } else if (filename === 'move_ticks.csv') {
+          moveTicksData.push(dataItems[j])
+        }
+      }
+      if (filename === 'sleep_ticks.csv' && index === sleepIdentifiers.length) {
+        convertAndFlush(sleepTicksData, headers, filename, false)
+      } else if (filename === 'move_ticks.csv' && index === moveIdentifiers.length) {
+        convertAndFlush(moveTicksData, headers, filename, false)
+      }
+    }
+  }
+}
+
+function createSummaryObjects (jsonArray) {
+  jsonArray.forEach(function (entry) {
+    var dailyDataJsonArray = dataSummary.filter(function (value) {
+      return value.date === entry.date
+    })
+
+    if (dailyDataJsonArray.length === 0) {
+      var dailyDataJsonObject = {}
+      dailyDataJsonObject.date = entry.date
+      dailyDataJsonObject.resting_heartrate = ''
+      dailyDataJsonObject.step_count = ''
+      dailyDataJsonObject.sleep_duration = ''
+      var newDailyDataJsonObject = true
     } else {
-        var dataItems = JSON.parse(body).data.items;
-        if (dataItems) {
-            for (var j = 0; j < dataItems.length; j++) {
-                dataItems[j]['user_xid'] = JSON.parse(body).meta['user_xid'];
-                dataItems[j]['time_accessed'] = JSON.parse(body).meta['time'];
-                dataItems[j]['user_email'] = userEmail;
-                dataItems[j]['ema_id'] = emaID;
-                if (filename === 'sleep_ticks.csv') {
-                    sleepTicksData.push(dataItems[j]);
-                } else if (filename === 'move_ticks.csv') {
-                    moveTicksData.push(dataItems[j]);
-                }
-            }
-            if (filename === 'sleep_ticks.csv' && index == sleepIdentifiers.length) {
-                convertAndFlush(sleepTicksData, headers, filename, false);
-            } else if (filename === 'move_ticks.csv' && index == moveIdentifiers.length) {
-                convertAndFlush(moveTicksData, headers, filename, false);
-            }
-        }
+      dailyDataJsonObject = dailyDataJsonArray[0]
+      newDailyDataJsonObject = false
     }
-}
 
-function createSummaryObjects(jsonArray) {
-    jsonArray.forEach(function(entry) {
-        var dailyDataJsonArray = dataSummary.filter(function(value) {
-            return value.date == entry.date;
-        });
-
-        if (dailyDataJsonArray.length == 0) {
-            var dailyDataJsonObject = {};
-            dailyDataJsonObject.date = entry.date;
-            dailyDataJsonObject.resting_heartrate = '';
-            dailyDataJsonObject.step_count = '';
-            dailyDataJsonObject.sleep_duration = '';
-            var newDailyDataJsonObject = true;
-        } else {
-            dailyDataJsonObject = dailyDataJsonArray[0];
-            newDailyDataJsonObject = false;
-        }
-
-        if (entry.resting_heartrate != null) {
-            dailyDataJsonObject.resting_heartrate = entry.resting_heartrate;
-        }
-        if (entry.details.steps != null) {
-            dailyDataJsonObject.step_count = entry.details.steps;
-        }
-        if (entry.details.duration != null) {
-            dailyDataJsonObject.sleep_duration = formatSeconds(entry.details.duration);
-        }
-
-        if (newDailyDataJsonObject) {
-            dataSummary.push(dailyDataJsonObject);
-        }
-    });
-
-    summaryCounter++;
-    if (summaryCounter == 3) {
-        dataSummary.sort(compare);
-        convertAndFlush(dataSummary, SUMMARY_HEADERS, 'summary.csv', false);
+    if (entry.resting_heartrate != null) {
+      dailyDataJsonObject.resting_heartrate = entry.resting_heartrate
     }
+    if (entry.details.steps != null) {
+      dailyDataJsonObject.step_count = entry.details.steps
+    }
+    if (entry.details.duration != null) {
+      dailyDataJsonObject.sleep_duration = formatSeconds(entry.details.duration)
+    }
+
+    if (newDailyDataJsonObject) {
+      dataSummary.push(dailyDataJsonObject)
+    }
+  })
+
+  summaryCounter++
+  if (summaryCounter === 3) {
+    dataSummary.sort(compare)
+    convertAndFlush(dataSummary, SUMMARY_HEADERS, 'summary.csv', false)
+  }
 }
 
-function formatSeconds(durationInSeconds) {
-    var hours = Math.floor(parseInt(durationInSeconds) / 3600);
-    durationInSeconds %= 3600;
-    var minutes = Math.floor(parseInt(durationInSeconds) / 60);
-    return hours + "h " + minutes + "m";
+function formatSeconds (durationInSeconds) {
+  var hours = Math.floor(parseInt(durationInSeconds) / 3600)
+  durationInSeconds %= 3600
+  var minutes = Math.floor(parseInt(durationInSeconds) / 60)
+  return hours + 'h ' + minutes + 'm'
 }
 
-function compare(objA, objB) {
-    if (objA.date < objB.date)
-        return -1;
-    else if (objA.date > objB.date)
-        return 1;
-    else
-        return 0;
+function compare (objA, objB) {
+  if (objA.date < objB.date) {
+    return -1
+  } else if (objA.date > objB.date) {
+    return 1
+  } else {
+    return 0
+  }
 }
